87935f3ef87f61fdd03986d8d6e3ce8d6c3d536d
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-23 10:27:23 +0100

    add Autowired on FotoRepo.

--- src/main/java/com/experis/course/fotoalbum/controller/FotoController.java
+++ src/main/java/com/experis/course/fotoalbum/controller/FotoController.java
@@ -1,15 +1,24 @@
 package com.experis.course.fotoalbum.controller;
 
+import com.experis.course.fotoalbum.model.Foto;
+import com.experis.course.fotoalbum.repository.FotoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/fotos")
 public class FotoController {
+    // ATTRIBUTI
+    @Autowired
+    private FotoRepository fotoRepository;
     // Index
     @GetMapping
     public String index() {
+        List<Foto> fotoList = null;
         return "fotos/index";
     }
 }
