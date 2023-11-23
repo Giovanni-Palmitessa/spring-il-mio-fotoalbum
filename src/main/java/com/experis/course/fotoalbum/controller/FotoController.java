c67704611b2b91189dbe86e0c92f2853540f88fc
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-23 10:22:11 +0100

    add request mapping and controller annotation

---
+++ src/main/java/com/experis/course/fotoalbum/controller/FotoController.java
@@ -0,0 +1,9 @@
+package com.experis.course.fotoalbum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/fotos")
+public class FotoController {
+}
