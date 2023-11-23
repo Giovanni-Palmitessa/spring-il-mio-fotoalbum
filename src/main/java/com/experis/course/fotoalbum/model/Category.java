9a0883404fb4fffb5b5b871fd977a21da98dc087
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-23 17:09:11 +0100

    add attributes

--- src/main/java/com/experis/course/fotoalbum/model/Category.java
+++ src/main/java/com/experis/course/fotoalbum/model/Category.java
@@ -1,9 +1,22 @@
 package com.experis.course.fotoalbum.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "categories")
 public class Category {
+    // Attributi
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    @Size(max = 50)
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Lob
+    private String description;
 }
