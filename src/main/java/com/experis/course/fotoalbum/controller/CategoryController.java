63f6de21f0a259707f32a83ac2e70b3c1dba9c50
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-23 18:17:33 +0100

    dep. inj. on category seevice

--- src/main/java/com/experis/course/fotoalbum/controller/CategoryController.java
+++ src/main/java/com/experis/course/fotoalbum/controller/CategoryController.java
@@ -1,7 +1,13 @@
 package com.experis.course.fotoalbum.controller;
 
+import com.experis.course.fotoalbum.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/categories")
 public class CategoryController {
+    @Autowired
+    CategoryService categoryService;
 }
