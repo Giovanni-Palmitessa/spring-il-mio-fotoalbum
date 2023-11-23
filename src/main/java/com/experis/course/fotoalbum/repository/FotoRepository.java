e3878a512dc52d229c4bafb5dff66845779476c0
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-23 12:11:05 +0100

    add custom query string

--- src/main/java/com/experis/course/fotoalbum/repository/FotoRepository.java
+++ src/main/java/com/experis/course/fotoalbum/repository/FotoRepository.java
@@ -3,5 +3,9 @@ package com.experis.course.fotoalbum.repository;
 import com.experis.course.fotoalbum.model.Foto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FotoRepository extends JpaRepository<Foto, Integer> {
+    // query che filtra le foto per titolo
+    List<Foto> findByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String titleKeyword, String authorsKeyword);
 }
