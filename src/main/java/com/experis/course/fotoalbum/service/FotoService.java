920091deefba6a88cbb2d7d8fbe024a1ce0dc731
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-23 15:46:54 +0100

    add autowired on fotoRepository

--- src/main/java/com/experis/course/fotoalbum/service/FotoService.java
+++ src/main/java/com/experis/course/fotoalbum/service/FotoService.java
@@ -1,7 +1,13 @@
 package com.experis.course.fotoalbum.service;
 
+import com.experis.course.fotoalbum.repository.FotoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FotoService {
+    // Repository
+
+    @Autowired
+    private FotoRepository fotoRepository;
 }
