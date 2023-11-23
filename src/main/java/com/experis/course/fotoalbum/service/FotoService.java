4f5a0e6c6a3b4b27786008e0e65f1b7c0b8c7d77
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-23 15:52:06 +0100

    add method getFotoList

--- src/main/java/com/experis/course/fotoalbum/service/FotoService.java
+++ src/main/java/com/experis/course/fotoalbum/service/FotoService.java
@@ -1,8 +1,12 @@
 package com.experis.course.fotoalbum.service;
 
+import com.experis.course.fotoalbum.model.Foto;
 import com.experis.course.fotoalbum.repository.FotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FotoService {
@@ -10,4 +14,17 @@ public class FotoService {
 
     @Autowired
     private FotoRepository fotoRepository;
+
+    // Metodi
+
+    // metodo che restituisce la lista di foto con o senza filtri
+    public List<Foto> getFotoList(Optional<String> search) {
+        if (search.isPresent()) {
+            // se il parametro search è presente chiamo il metodo custom
+            return fotoRepository.findByTitleContainingIgnoreCaseOrDescriptionContaining(search.get(), search.get());
+        } else {
+            // se il parametro search non è presente mostro tutte le foto
+            return fotoRepository.findAll();
+        }
+    }
 }
