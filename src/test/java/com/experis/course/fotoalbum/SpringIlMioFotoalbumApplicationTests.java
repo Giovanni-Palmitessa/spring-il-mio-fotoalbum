a5ed7f51cd21d94c5b0c7e9dd1adf436e47f24c0
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-23 09:45:13 +0100

    Initial commit

---
+++ src/test/java/com/experis/course/fotoalbum/SpringIlMioFotoalbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experis.course.fotoalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIlMioFotoalbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
