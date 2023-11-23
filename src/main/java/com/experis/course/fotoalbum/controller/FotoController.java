09b79a27f9a73b96ba6a972a1c26eec036f97d6a
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-23 10:23:02 +0100

    add index method

--- src/main/java/com/experis/course/fotoalbum/controller/FotoController.java
+++ src/main/java/com/experis/course/fotoalbum/controller/FotoController.java
@@ -1,9 +1,15 @@
 package com.experis.course.fotoalbum.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/fotos")
 public class FotoController {
+    // Index
+    @GetMapping
+    public String index() {
+        return "fotos/index";
+    }
 }
