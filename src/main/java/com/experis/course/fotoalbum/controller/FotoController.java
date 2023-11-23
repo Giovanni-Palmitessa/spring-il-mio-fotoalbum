79e1db81c348c44d665487c78a8dfde9b67801e3
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-23 10:30:50 +0100

    add model add attribute

--- src/main/java/com/experis/course/fotoalbum/controller/FotoController.java
+++ src/main/java/com/experis/course/fotoalbum/controller/FotoController.java
@@ -4,6 +4,7 @@ import com.experis.course.fotoalbum.model.Foto;
 import com.experis.course.fotoalbum.repository.FotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,8 +19,11 @@ public class FotoController {
 
     // Index
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        // creo lista di foto
         List<Foto> fotoList = fotoRepository.findAll();
+        // passo al template la lista di Foto
+        model.addAttribute("fotoList", fotoList);
         return "fotos/index";
     }
 }
