cb62d1d5216c9a04d3875a534cc9aa7dc9edc49b
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-23 11:46:10 +0100

    test for show

--- src/main/java/com/experis/course/fotoalbum/controller/FotoController.java
+++ src/main/java/com/experis/course/fotoalbum/controller/FotoController.java
@@ -3,13 +3,16 @@ package com.experis.course.fotoalbum.controller;
 import com.experis.course.fotoalbum.model.Foto;
 import com.experis.course.fotoalbum.repository.FotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/fotos")
@@ -30,7 +33,18 @@ public class FotoController {
 
     // Show che mostra il libro preso per id
     @GetMapping("/show/{id}")
-    public String show(@PathVariable Integer id) {
-        return "fotos/show";
+    public String show(@PathVariable Integer id, Model model) {
+        // aggiungo un opzionale poichè potrei trovare la foto o no
+        Optional<Foto> result = fotoRepository.findById(id);
+        // verifico se il risultato è presente
+        if (result.isPresent()){
+            // gli passo l'id con model attribute
+            model.addAttribute("fotos", result.get());
+            return "fotos/show";
+        } else {
+            // se non trovo la foto sollevo eccezione
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "La foto con id: " + id + " non è stata trovata!");
+        }
+
     }
 }
