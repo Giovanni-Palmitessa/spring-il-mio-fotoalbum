07d80fbe15afa83a4eb51efbc5532623d554052a
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-23 09:55:17 +0100

    add basic attributes

--- src/main/java/com/experis/course/fotoalbum/model/Foto.java
+++ src/main/java/com/experis/course/fotoalbum/model/Foto.java
@@ -1,9 +1,17 @@
 package com.experis.course.fotoalbum.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "fotos")
 public class Foto {
+    // ATTRIBUTI
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private boolean visible;
+
 }
