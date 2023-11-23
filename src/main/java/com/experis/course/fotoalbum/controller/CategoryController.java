6273a0fbc1c3845284e4adb2ddfda3d4abc98933
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-23 18:26:08 +0100

    add get mapping and method index

--- src/main/java/com/experis/course/fotoalbum/controller/CategoryController.java
+++ src/main/java/com/experis/course/fotoalbum/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.experis.course.fotoalbum.controller;
 import com.experis.course.fotoalbum.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +14,7 @@ public class CategoryController {
     CategoryService categoryService;
 
     // Metodi
+    @GetMapping
     public String index() {
         return "categories/index";
     }
