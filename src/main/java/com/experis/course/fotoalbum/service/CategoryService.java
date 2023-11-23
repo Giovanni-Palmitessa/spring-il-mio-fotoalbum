7812dbda8c21e96bef4c00bf31ee6ea82f5f6ac2
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-23 17:28:43 +0100

    add category service

--- src/main/java/com/experis/course/fotoalbum/service/CategoryService.java
+++ src/main/java/com/experis/course/fotoalbum/service/CategoryService.java
@@ -1,4 +1,22 @@
 package com.experis.course.fotoalbum.service;
 
+import com.experis.course.fotoalbum.model.Category;
+import com.experis.course.fotoalbum.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoryService {
+    // ATTRIBUTI
+    // istanzio category repos.
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    // METODI
+    // metodo che mi restituisce la lista di tutte le categorie
+    public List<Category> getAllCategories() {
+        return categoryRepository.findByOrderByName();
+    }
 }
