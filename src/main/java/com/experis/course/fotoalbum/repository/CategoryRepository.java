dd311c576ef4a20e98bc52ae98ce4a820b7212b5
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-23 17:27:53 +0100

    add method for order by name

--- src/main/java/com/experis/course/fotoalbum/repository/CategoryRepository.java
+++ src/main/java/com/experis/course/fotoalbum/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.experis.course.fotoalbum.repository;
 import com.experis.course.fotoalbum.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    List<Category> findByOrderByName();
 }
