362f49bc1bb7dd96a9526543f61f8d9f7accc9ce
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-23 09:56:07 +0100

    add getter and setter

--- src/main/java/com/experis/course/fotoalbum/model/Foto.java
+++ src/main/java/com/experis/course/fotoalbum/model/Foto.java
@@ -14,4 +14,44 @@ public class Foto {
     private String imageUrl;
     private boolean visible;
 
+    // GETTER E SETTER
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
 }
