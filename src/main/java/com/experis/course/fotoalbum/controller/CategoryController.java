9e542f124f0bdcce756c135fb21fffcddb83af16
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-24 09:10:04 +0100

    add method post to save categories

--- src/main/java/com/experis/course/fotoalbum/controller/CategoryController.java
+++ src/main/java/com/experis/course/fotoalbum/controller/CategoryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,4 +26,8 @@ public class CategoryController {
         return "categories/index";
     }
 
+    @PostMapping("/save")
+    public String save(Category formCategory) {
+
+    }
 }
