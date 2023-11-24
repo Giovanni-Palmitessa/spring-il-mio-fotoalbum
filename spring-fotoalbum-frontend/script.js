3cf6c1c6a40128aeedf7c7bc858e5084d1605904
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-24 16:37:53 +0100

    add index and script for frontend

--- spring-fotoalbum-frontend/script.js
+++ spring-fotoalbum-frontend/script.js
@@ -1,73 +1,67 @@
-/* COSTANTI*/
-const apiUrl = "http://localhost:8080/api/fotos";
+const baseUrl = "http://localhost:8080/api/photos";
 const root = document.getElementById("root");
 
-/* FUNZIONI */
-// funzione che renderizza le categorie delle foto
-const renderCategories = (categories) => {
+// Funzione per ottenere l'array delle foto
+const getPhoto = async (searchName = "") => {
+  try {
+    const url = `${baseUrl}?search=${searchName}`;
+    const response = await axios.get(url);
+    if (Array.isArray(response.data)) {
+      renderPhotoList(response.data); // Assicurati che response.data sia l'array di foto
+    } else {
+      console.error("Data format is incorrect:", response.data);
+    }
+  } catch (error) {
+    console.error("Error in getPhoto:", error);
+  }
+};
+
+// Funzione per la ricerca
+const searchPhoto = () => {
+  const searchName = document.getElementById("searchNameField").value;
+  getPhoto(searchName);
+};
+
+// Funzione per renderizzare ogni foto nell'elenco
+const renderPhoto = (element) => {
+  return `
+    <div class="card" style="width: 15rem; height: 10rem">
+      <img src="${element.urlImage}" class="card-img-top" alt="${
+    element.titolo
+  }">
+      <div class="card-body">
+        <h5 class="card-title">${element.titolo}</h5>
+        <p class="card-text">${element.descrizione}</p>
+        <div class="card-footer">${renderCategory(element.categories)}</div>
+        </div>
+        </div>`;
+};
+// <a href="show.html?id=${element.id}" class="card-link">Dettagli</a>
+
+const renderCategory = (categories) => {
   let content;
-  //   controllo se categorie sono uguali a 0
-  if (categories.length === 0) {
+  if (!categories || categories.length === 0) {
     content = "No categories";
   } else {
-    // se categorie presenti mostro lista categorie
-    content = '<ul class="list-unstyled">';
-    categories.forEach((cat) => {
-      content += `<li>${cat.name}</li>`;
+    content =
+      '<h3 class="text-danger">Categories</h3><ul class="list-unstyled">';
+    categories.forEach((category) => {
+      content += `<li>${category.name}</li>`;
     });
     content += "</ul>";
   }
   return content;
 };
 
-// funzione che renderizza la card della foto
-const renderFoto = (element) => {
-  return `<div class="card mb-5" style="width: 18rem;">
-      <img src="${
-        element.imageUrl
-      }" class="card-img-top" alt="Foto Image" style="height: 300px">
-      <div class="card-body">
-        <h5 class="card-title">${element.title}</h5>
-        <p class="card-text">${element.description}</p>
-      </div>
-      <div class="card-body">
-          ${renderCategories(element.categories)}
-      </div>
-    </div>`;
-};
-
-// funzione che renderizza la gallery di card
-const renderFotoList = (data) => {
-  let content;
-  if (data.length > 0) {
-    // creo la gallery
-    content = '<div class="row">';
-    // itero sull'array di foto
-    data.forEach((element) => {
-      content += '<div class="col-3">';
-      // chiamo il metodo che restituisce la card della foto
-      content += renderFoto(element);
-      content += "</div>";
-    });
-    content += "</div>";
-  } else {
-    content = '<div class="alert alert-info">La lista è vuota</div>';
-  }
-  // sostituisco il contenuto di root con il mio content
+// Funzione per renderizzare l'elenco delle foto
+const renderPhotoList = (data) => {
+  let content = '<div class="row">';
+  data.forEach((element) => {
+    content += `<div class="col-4 mt-3">${renderPhoto(element)}</div>`;
+  });
+  content += "</div>";
   root.innerHTML = content;
 };
 
-// funzione che chiama l'api e ottiene il json con l'array di books
-const getBooks = async () => {
-  try {
-    // ottengo la response dell'api
-    const response = await axios.get(apiUrl);
-    // passo i dati alla funzione che li renderizza
-    renderBookList(response.data);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-/* codice globale che viene eseguito al load dello script */
-getBooks();
+// Esegui la funzione per ottenere l'elenco delle foto all'avvio
+getPhoto();
