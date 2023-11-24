3ecc4265e5126c203ad18e8320f15196cbbb1605
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-24 10:54:07 +0100

    add method for find email

--- src/main/java/com/experis/course/fotoalbum/security/DatabaseUserDetailsService.java
+++ src/main/java/com/experis/course/fotoalbum/security/DatabaseUserDetailsService.java
@@ -1,11 +1,14 @@
 package com.experis.course.fotoalbum.security;
 
+import com.experis.course.fotoalbum.model.User;
 import com.experis.course.fotoalbum.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class DatabaseUserDetailsService implements UserDetailsService {
@@ -16,6 +19,14 @@ public class DatabaseUserDetailsService implements UserDetailsService {
     // Override metodi
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        // prendo la mail che l'utente inserisce e lo cerco su db
+        Optional<User> loggedUser = userRepository.findByEmail(username);
+        if (loggedUser.isPresent()){
+            // utente trovato
+            return new DatabaseUserDetails(loggedUser.get());
+        } else {
+            // L'utente non c'è sollevo eccezione
+            throw new UsernameNotFoundException(username);
+        }
     }
 }
