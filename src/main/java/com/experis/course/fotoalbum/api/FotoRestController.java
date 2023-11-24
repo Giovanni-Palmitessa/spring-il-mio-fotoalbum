4ce31e1d11c0e546c2549a813c2ab90622f8c283
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-24 12:42:53 +0100

    add show method for api

--- src/main/java/com/experis/course/fotoalbum/api/FotoRestController.java
+++ src/main/java/com/experis/course/fotoalbum/api/FotoRestController.java
@@ -1,9 +1,12 @@
 package com.experis.course.fotoalbum.api;
 
+import com.experis.course.fotoalbum.exceptions.FotoNotFoundException;
 import com.experis.course.fotoalbum.model.Foto;
 import com.experis.course.fotoalbum.service.FotoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,6 +25,18 @@ public class FotoRestController {
     @GetMapping
     public List<Foto> index(@RequestParam Optional<String> search) {
         return fotoService.getFotoList(search);
+    }
+
+    // endpoint per i dettagli del libro preso per id
+    @GetMapping("/{id}")
+    public Foto details(@PathVariable Integer id) {
+        try {
+            // se trovo la foto con id mostro i dettagli
+            return fotoService.getFotoById(id);
+        } catch (FotoNotFoundException e) {
+            // tiro un eccezione
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "La foto con id: " + id + " non è stata trovata!");
+        }
     }
 
 }
