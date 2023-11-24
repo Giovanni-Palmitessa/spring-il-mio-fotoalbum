b3be535fa7050048b84b8da74da76f4d802ce0a5
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-24 11:28:53 +0100

    add securityChain

--- src/main/java/com/experis/course/fotoalbum/security/SecurityConfiguration.java
+++ src/main/java/com/experis/course/fotoalbum/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.experis.course.fotoalbum.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -39,10 +40,12 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .authorizeHttpRequests()
-                .anyRequest()
-                .authenticated().and()
-                .formLogin().and()
-                .logout();
+                .requestMatchers("/categories").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.POST).hasAuthority("ADMIN")
+                .requestMatchers("/fotos").hasAuthority("ADMIN")
+                .requestMatchers("/**").permitAll()
+                .and().formLogin()
+                .and().logout();
         return http.build();
     }
 }
