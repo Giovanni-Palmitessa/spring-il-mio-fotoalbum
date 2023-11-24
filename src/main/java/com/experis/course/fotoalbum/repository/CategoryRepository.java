1c8a777d139f5a5b2cdcf9e55db14feaa5c75639
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-24 09:34:57 +0100

    add method find by name

--- src/main/java/com/experis/course/fotoalbum/repository/CategoryRepository.java
+++ src/main/java/com/experis/course/fotoalbum/repository/CategoryRepository.java
@@ -4,12 +4,11 @@ import com.experis.course.fotoalbum.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
     // metodo per cercare le categorie e ordinarle
     List<Category> findByOrderByName();
 
     // metodo per cercare le categorie per nome
-    Optional<Category> findByName(String name);
+    boolean existByName(String name);
 }
