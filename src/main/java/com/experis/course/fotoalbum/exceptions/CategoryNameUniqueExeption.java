3325b0d680d658e480365d7e7bab7bd2a29b0ca8
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-24 09:39:00 +0100

    add exeption for unique category

---
+++ src/main/java/com/experis/course/fotoalbum/exceptions/CategoryNameUniqueExeption.java
@@ -0,0 +1,7 @@
+package com.experis.course.fotoalbum.exceptions;
+
+public class CategoryNameUniqueExeption extends RuntimeException{
+    public CategoryNameUniqueExeption(String message) {
+        super(message);
+    }
+}
