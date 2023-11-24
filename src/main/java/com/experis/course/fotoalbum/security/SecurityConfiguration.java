5180302edb4b30dfdd73813a31b25948d3841583
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-24 11:03:01 +0100

    add password encoder

--- src/main/java/com/experis/course/fotoalbum/security/SecurityConfiguration.java
+++ src/main/java/com/experis/course/fotoalbum/security/SecurityConfiguration.java
@@ -2,12 +2,33 @@ package com.experis.course.fotoalbum.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration {
     // metodi
+    // configurazione su come avere user detail service
     @Bean
     public DatabaseUserDetailsService userDetailsService(){
         return new DatabaseUserDetailsService();
+    }
+
+    // configurazione su come avere passwordEncoder
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    // configurazione AuthenticationProvider
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        // assegno user detailsService
+        provider.setUserDetailsService(userDetailsService());
+        // assegno password encoder
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 }
