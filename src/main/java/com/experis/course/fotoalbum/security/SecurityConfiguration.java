03a2d269ed2710f60c513ec406b0ea1ffcaf54a4
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-24 11:09:12 +0100

    add security filter chain

--- src/main/java/com/experis/course/fotoalbum/security/SecurityConfiguration.java
+++ src/main/java/com/experis/course/fotoalbum/security/SecurityConfiguration.java
@@ -3,8 +3,10 @@ package com.experis.course.fotoalbum.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
@@ -30,5 +32,10 @@ public class SecurityConfiguration {
         // assegno password encoder
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
+    }
+
+    // Security Filteer chain
+    public SecurityFilterChain filterChain(HttpSecurity http){
+
     }
 }
