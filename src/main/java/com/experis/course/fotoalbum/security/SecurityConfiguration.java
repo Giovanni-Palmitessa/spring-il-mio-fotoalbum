0f9226fd09af96ff6a45e16596dee9635afac5fc
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-24 10:57:43 +0100

    add ,ethod for return user details

--- src/main/java/com/experis/course/fotoalbum/security/SecurityConfiguration.java
+++ src/main/java/com/experis/course/fotoalbum/security/SecurityConfiguration.java
@@ -1,7 +1,13 @@
 package com.experis.course.fotoalbum.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SecurityConfiguration {
+    // metodi
+    @Bean
+    DatabaseUserDetailsService userDetailsService(){
+        return new DatabaseUserDetailsService();
+    }
 }
