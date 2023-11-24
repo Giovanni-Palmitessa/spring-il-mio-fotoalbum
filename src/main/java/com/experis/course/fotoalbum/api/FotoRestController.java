e1b7a9b68d872f38f29f732567cdc5be0e3784e7
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-24 12:16:41 +0100

    add foto RestController

--- src/main/java/com/experis/course/fotoalbum/api/FotoRestController.java
+++ src/main/java/com/experis/course/fotoalbum/api/FotoRestController.java
@@ -1,7 +1,11 @@
 package com.experis.course.fotoalbum.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/fotos")
+@CrossOrigin
 public class FotoRestController {
 }
