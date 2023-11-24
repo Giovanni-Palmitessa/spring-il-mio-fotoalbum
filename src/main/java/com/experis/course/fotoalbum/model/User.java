9c945a6414b4f7e523f01765605a892708eeba11
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-24 10:11:19 +0100

    add attributes

--- src/main/java/com/experis/course/fotoalbum/model/User.java
+++ src/main/java/com/experis/course/fotoalbum/model/User.java
@@ -1,9 +1,26 @@
 package com.experis.course.fotoalbum.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
 public class User {
+    // Attributi
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NotBlank
+    @Email
+    private String email;
+    private LocalDateTime registeredAt;
+    private String password;
 }
