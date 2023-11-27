fb846c91a64dcc2b7223f436266a60f756a93e83
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-27 16:40:43 +0100

    add contact controller

--- src/main/java/com/experis/course/fotoalbum/controller/ContactController.java
+++ src/main/java/com/experis/course/fotoalbum/controller/ContactController.java
@@ -1,9 +1,14 @@
 package com.experis.course.fotoalbum.controller;
 
+import com.experis.course.fotoalbum.model.Contact;
 import com.experis.course.fotoalbum.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/contacts")
@@ -11,4 +16,11 @@ public class ContactController {
     // dep injection
     @Autowired
     ContactRepository contactRepository;
+
+    @GetMapping
+    public String index(Model model) {
+        List<Contact> contacts = contactRepository.findAll();
+        model.addAttribute("contacts", contacts);
+        return "contacts/list";
+    }
 }
